002c38b589575fb91ff50bb61bad6c127bdaaaff
jxnbox <janr3p@gmail.com>
2023-03-27 19:41:51 +0100

    adding test data

---
+++ db/data/test-data/invoices.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        name : "Test One",
+        date_of_birth : "01/05/1970",
+        report_type : "medical form",
+        price: 40,
+        invoice_number: 7356,
+        paid: true,
+        assigned: "Dr Allen",
+        date : 1679942030000
+    },
+    {
+        name : "Test Two",
+        date_of_birth : "12/10/1940",
+        report_type : "medical form",
+        price: 40,
+        invoice_number: 7506,
+        paid: true,
+        assigned: "Dr Allen",
+        date : 1680178850000
+    },
+    {
+        name : "Test Three",
+        date_of_birth : "12/10/1940",
+        report_type : "sports form",
+        price: 50,
+        invoice_number: 7507,
+        paid: true,
+        assigned: "Dr Hallet",
+        date : 1680189650000
+    },
+    {
+        name : "Test Four",
+        date_of_birth : "12/10/1940",
+        report_type : "sports form",
+        price: 50,
+        invoice_number: 7653,
+        paid: true,
+        assigned: "Dr Allen",
+        date : 1701512123000
+    },
+    {
+        name : "Test Five",
+        date_of_birth : "12/10/1940",
+        report_type : "MAR",
+        price: 200,
+        invoice_number: 6334,
+        paid: true,
+        assigned: "Dr Ubaysiri",
+        date : 1673007994000
+    },
+]
